3d041852f60194227d5859c448ff0f05ae1ac56e
balazskreith <kreith.balazs@gmail.com>
2024-10-29 08:52:47 +0200

    save

--- webapp/src/signals/signals.tsx
+++ webapp/src/signals/signals.tsx
@@ -11,31 +11,3 @@ export type Page =
 export const [ page, setPage ] = createSignal<Page>('lobby');
 
 export type MediaResults = 'tcp' | 'relay' | 'bw' | 'loss' | 'latency' | 'connection';
-
-type MediaDevice = { deviceId: string; kind: MediaDeviceKind; label: string; }
-
-export const [ participantId, setParticipantId ] = createSignal<string>();
-export const [ slugId, setSlugId ] = createSignal<string>();
-
-export const [ availableOutgoingBitrates, setAvailableOutgoingBitrates ] = createSignal<number[]>([]);
-
-export const [ incomingBitrate, setIncomingBitrate ] = createSignal<number[]>([]);
-export const [ outgoingBitrate, setOutgoingBitrate ] = createSignal<number[]>([]);
-
-export const [ packetLoss, setPacketLoss ] = createSignal<number[]>([]);
-export const [ rtt, setRtt ] = createSignal<number[]>([]);
-
-export const [ webcamTrack, setWebcamTrack ] = createSignal<MediaStreamTrack>();
-export const [ micTrack, setMicTrack ] = createSignal<MediaStreamTrack>();
-
-export const [ videoDevices, setVideoDevices ] = createSignal<MediaDevice[]>([]);
-export const [ audioDevices, setAudioDevices ] = createSignal<MediaDevice[]>([]);
-
-export const [ videoDeviceId, setVideoDeviceId ] = createSignal<string>();
-export const [ audioDeviceId, setAudioDeviceId ] = createSignal<string>();
-
-export const [ lowBandwidth, setLowBandwidth ] = createSignal<boolean>(false);
-export const [ highLatency, setHighLatency ] = createSignal<boolean>(false);
-export const [ highPacketLoss, setHighPacketLoss ] = createSignal<boolean>(false);
-export const [ usingRelay, setUsingRelay ] = createSignal<boolean>(false);
-export const [ usingTCP, setUsingTCP ] = createSignal<boolean>(false);
