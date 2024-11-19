c0e9e91f1dc5bc0ebd161c7a014e3b3924ef5a61
Tamas Levai <levait@tmit.bme.hu>
2024-11-19 17:13:57 +0100

    Add stunner metrics grafana dashboard

--- webapp/src/views/StunnerPage.tsx
+++ webapp/src/views/StunnerPage.tsx
@@ -16,13 +16,18 @@ const StunnerPage: Component = () => {
 		<Grid container spacing={2}>
 			<Grid item xs={12}>
 				<Section title="STUNner">
-                    More about stunner
+
+				More about STUNner...
+
+				STUNner metrics
+				<iframe src="https://www.webrtc-observer.org/g/public-dashboards/c3f4bd864c094ad49396aab48a9aea09?from=now-5m&to=now&timezone=browser" title="STUNner Metrics"></iframe>
+
 				</Section>
 
 				<Section>
 					<a href="#" class="text-sm text-blue-600 dark:text-blue-500 hover:underline" onClick={() => setPage('main')}>Back</a>
 				</Section>
-				
+
 			</Grid>
 		</Grid>
 	);
