831a5dc01c45ccc23e9ca37c59140a0c5aa9afd8
Tamas Levai <levait@tmit.bme.hu>
2024-11-20 11:15:14 +0100

    Add content to the STUNner page

--- webapp/src/views/StunnerPage.tsx
+++ webapp/src/views/StunnerPage.tsx
@@ -16,11 +16,15 @@ const StunnerPage: Component = () => {
 		<Grid container spacing={2}>
 			<Grid item xs={12}>
 				<Section title="STUNner">
+				<p>
+				<a href="https://github.com/l7mp/stunner">STUNner</a> allows you to deploy any WebRTC service into Kubernetes, smoothly integrating it into the cloud-native ecosystem. STUNner exposes a standards-compliant STUN/TURN gateway for clients to access your virtualized WebRTC infrastructure running in Kubernetes, maintaining full browser compatibility and requiring minimal or no modification to your existing WebRTC codebase. STUNner supports the Kubernetes Gateway API so you can configure it in the familiar YAML-engineering style via Kubernetes manifests.
+				</p>
 
-				More about STUNner...
+				<p>
+				STUNner exposes various statistics in an external time-series database like Prometheus. This way, one can observe the state of the STUNner media gateway instances, such as CPU or memory use or the amount of data received and sent in quasi-real-time. Below, we present a Grafana dashboard showing some of the key STUNner performance metrics.
 
-				STUNner metrics
 				<iframe src="https://www.webrtc-observer.org/g/public-dashboards/c3f4bd864c094ad49396aab48a9aea09?from=now-5m&to=now&timezone=browser&theme=light" title="STUNner Metrics" height="480"></iframe>
+				</p>
 
 				</Section>
 
